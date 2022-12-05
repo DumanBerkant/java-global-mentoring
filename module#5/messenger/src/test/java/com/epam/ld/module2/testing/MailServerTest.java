cc2938548d5de83b94806c91fe19c9d16f210cbb
Berkant <Berkant_Duman@epam.com>
2022-12-05 13:33:53 +0300

    module#5 initialized and test cases added

---
+++ module#5/messenger/src/test/java/com/epam/ld/module2/testing/MailServerTest.java
@@ -0,0 +1,13 @@
+package com.epam.ld.module2.testing;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MailServerTest {
+
+    @Test
+    public void newTestFunction(){
+        assertTrue(true);
+    }
+
+}
---
+++ module#5/messenger/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -0,0 +1,31 @@
+package com.epam.ld.module2.testing.template;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TemplateEngineTest {
+
+    @Test
+    public void When_RuntimeVariablesGiven_Expect_ReplaceVariablesWithPlaceholders(){
+        fail();
+    }
+
+    @Test
+    public void When_AtLeastOnePlaceholderVariableIsMissing_Expect_TemplateThrowAnException(){
+        fail();
+    }
+
+
+    @Test
+    public void When_VariableNotProvidedInTemplate_Expect_TemplateIgnoresTheValue(){
+        fail();
+    }
+
+    @Test
+    public void When_ReplacedThePlaceHolders_Expect_SystemSupportAllLatin1Chars(){
+        fail();
+    }
+
+
+}
